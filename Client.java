1a2c1cc522b957f6dddc24b5b53304da9b7b100c
Valney Faria <valneyfaria@hotmail.com>
2018-09-24 22:58:53 -0300

    Finalizando

--- Client.java
+++ Client.java
@@ -1,4 +1,8 @@
 import java.rmi.registry.Registry;
+import java.util.Scanner;
+
+import com.sun.xml.internal.ws.api.streaming.XMLStreamReaderFactory.Default;
+
 import java.rmi.registry.LocateRegistry;
 
 public class Client {
@@ -7,28 +11,67 @@ public class Client {
 	}
 
 	public static void main(String[] args) {
+		Scanner s = new Scanner();
+		int opt = 1, nCandidato = 0, nVotante = 0;
 
 		String host = (args.length < 1) ? null : args[0];
 		System.out.println("Endereco do servidor: " + host);
 		try {
 			Registry registry = LocateRegistry.getRegistry(host);
-			// Lookup busca quem tem a interface Eleicao em Registry. No Registry, existe uma
-			// tupla que
+			// Lookup busca quem tem a interface Eleicao em Registry.
+			// No Registry, existe uma tupla que
 			// armazena o stub e a sua label
 			Eleicao stub = (Eleicao) registry.lookup("Eleicao");
 			// Chama o Eleicao no servidor
-            String response = stub.sayHello();
+			String response = stub.sayHello();
 
 			System.out.println("response: " + response);
 
+			while (opt != 0) {
+				System.out.println("Selecione uma opção:\n");
+				System.out.println("1 - Votar num candidato\n");
+				System.out.println("2 - Ver o resultado de um candidato\n");
+				System.out.println("3 - Ver o resultado da eleição\n");
+				opt = s.nextInt();
 
+				switch (opt) {
+				case 1:
+					System.out.println("Insira o numero do candidato:");
+					nCandidato = s.nextInt();
+					System.out.println("Agora insira o seu numero:");
+					nVotante = s.nextInt();
 
-            votar(int nCandidato, int nVotante);
+					if (stub.votar(nCandidato, nVotante)) {
+						System.out.println("VOTAÇÃO CONCLUÍDA!");
+					} else {
+						System.out.println("ERRO NA VOTAÇÃO!");
+					}
 
-            resultadoParcial(int nCandidato);
+					break;
 
-            resultado();
+				case 2:
+					System.out.println("Insira o numero do candidato:");
+					nCandidato = s.nextInt();
 
+					if (stub.resultadoParcial(nCandidato)) {
+						System.out.println("OPERAÇÃO CONCLUÍDA COM SUCESSO!");
+					} else {
+						System.out.println("ERRO NA VOTAÇÃO!");
+					}
+
+					break;
+
+				case 3:
+					System.out.println("COMPUTANDO RESULTADO...");
+					stub.resultado();
+
+					break;
+
+				default:
+					System.out.println("ELEIÇÃO CONCLUÍDA!");
+					break;
+				}
+			}
 
 		} catch (Exception e) {
 			System.err.println("Client exception: " + e.toString());
